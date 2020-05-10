f43cbba23784a4dc7b33dd9a03ed088c4b63cc0c
Lawrence Jones <lawrjone@gmail.com>
2020-05-10 14:36:54 +0100

    Drop and restructure import_jobs
    We want to track imports against a subscription, not a publication.

---
+++ pkg/migration/20200510142527_create_import_jobs_table.go
@@ -0,0 +1,37 @@
+package migration
+
+import (
+	"database/sql"
+
+	"github.com/pressly/goose"
+)
+
+func init() {
+	goose.AddMigration(Up20200510142527, Down20200510142527)
+}
+
+// Recreate import jobs with a cleaner structure.
+func Up20200510142527(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+	create table pg2sink.import_jobs (
+		id bigserial primary key,
+		subscription_id text not null,
+		table_name text not null,
+		cursor text,
+		completed_at timestamptz,
+		expired_at timestamptz,
+		updated_at timestamptz not null default now(),
+		created_at timestamptz not null default now()
+	);
+	`)
+
+	return err
+}
+
+func Down20200510142527(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+	drop table pg2sink.import_jobs;
+	`)
+
+	return err
+}
