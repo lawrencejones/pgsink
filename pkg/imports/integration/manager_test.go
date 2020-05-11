21ff753aebbb1f2cb3ba6a35aefce9de10beeda0
Lawrence Jones <lawrjone@gmail.com>
2020-05-11 18:38:45 +0100

    Rewrite imports.Manager with tests
    Use go-jet to build queries and use the new database structure for
    import_jobs.

---
+++ pkg/imports/integration/manager_test.go
@@ -0,0 +1,144 @@
+package integration
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/lawrencejones/pg2sink/pkg/dbschema/model"
+	. "github.com/lawrencejones/pg2sink/pkg/dbschema/table"
+	"github.com/lawrencejones/pg2sink/pkg/imports"
+	"github.com/lawrencejones/pg2sink/pkg/subscription"
+
+	. "github.com/go-jet/jet/postgres"
+	_ "github.com/jackc/pgx/v4/stdlib"
+
+	. "github.com/onsi/ginkgo"
+	_ "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+)
+
+const (
+	schemaName      = "imports_manager_integration_test"
+	publicationName = "imports_manager_integration_test"
+	subscriptionID  = "uniqueness"
+)
+
+var (
+	tableOneName   = fmt.Sprintf("%s.%s", schemaName, "one")
+	tableTwoName   = fmt.Sprintf("%s.%s", schemaName, "two")
+	tableThreeName = fmt.Sprintf("%s.%s", schemaName, "three")
+)
+
+var _ = Describe("Manager", func() {
+	var (
+		ctx     context.Context
+		cancel  func()
+		db      *sql.DB
+		sub     subscription.Subscription
+		manager *imports.Manager
+		err     error
+	)
+
+	mustExec := func(sql string, args []interface{}, message ...interface{}) {
+		_, err := db.ExecContext(ctx, fmt.Sprintf(sql, args...))
+		Expect(err).To(BeNil(), message...)
+	}
+
+	JustBeforeEach(func() {
+		manager = imports.NewManager(logger, db, imports.ManagerOptions{})
+	})
+
+	BeforeEach(func() {
+		ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+
+		db, err = sql.Open("pgx", "")
+		Expect(err).NotTo(HaveOccurred(), "failed to connect to database")
+
+		// Remove any artifacts from previous runs
+		db.ExecContext(ctx, fmt.Sprintf(`drop schema if exists %s cascade`, schemaName))
+		db.ExecContext(ctx, fmt.Sprintf(`drop publication if exists %s`, publicationName))
+
+		// Recreate global state
+		db.ExecContext(ctx, fmt.Sprintf(`create schema %s`, schemaName))
+		db.ExecContext(ctx, fmt.Sprintf(`create publication %s`, publicationName))
+
+		sub = subscription.Subscription{
+			Publication: subscription.Publication{
+				ID:   subscriptionID,
+				Name: publicationName,
+			},
+		}
+	})
+
+	AfterEach(func() {
+		cancel()
+	})
+
+	Describe("Reconcile()", func() {
+		var (
+			jobs []model.ImportJobs
+			err  error
+		)
+
+		JustBeforeEach(func() {
+			jobs, err = manager.Reconcile(ctx, sub)
+		})
+
+		BeforeEach(func() {
+			mustExec(fmt.Sprintf(`create table %s (id bigserial primary key);`, tableOneName), nil)
+			mustExec(fmt.Sprintf(`create table %s (id bigserial primary key);`, tableTwoName), nil)
+		})
+
+		Context("for published table", func() {
+			BeforeEach(func() {
+				Expect(sub.SetTables(ctx, db, tableOneName)).To(Succeed())
+			})
+
+			Context("with no previous import", func() {
+				It("creates import job", func() {
+					Expect(err).NotTo(HaveOccurred())
+					Expect(jobs).To(ContainElement(
+						MatchFields(IgnoreExtras, Fields{"TableName": Equal(tableOneName)}),
+					))
+				})
+			})
+
+			Context("with previous import", func() {
+				BeforeEach(func() {
+					stmt := ImportJobs.
+						INSERT(ImportJobs.SubscriptionID, ImportJobs.TableName).
+						VALUES(sub.GetID(), tableOneName)
+
+					_, err := stmt.ExecContext(ctx, db)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				It("does not create an additional import", func() {
+					Expect(err).NotTo(HaveOccurred())
+					Expect(jobs).To(BeEmpty())
+				})
+			})
+
+			Context("with previous expired import", func() {
+				BeforeEach(func() {
+					stmt := ImportJobs.
+						INSERT(ImportJobs.SubscriptionID, ImportJobs.TableName, ImportJobs.ExpiredAt).
+						VALUES(sub.GetID(), tableOneName, Raw("now()"))
+
+					_, err := stmt.ExecContext(ctx, db)
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				It("creates import job", func() {
+					Expect(err).NotTo(HaveOccurred())
+					Expect(jobs).To(ContainElement(
+						MatchFields(IgnoreExtras, Fields{"TableName": Equal(tableOneName)}),
+					))
+				})
+			})
+		})
+	})
+})
---
+++ pkg/imports/integration/suite_test.go
@@ -0,0 +1,20 @@
+package integration
+
+import (
+	"testing"
+
+	kitlog "github.com/go-kit/kit/log"
+
+	// Ensure we migrate the database before running any tests
+	_ "github.com/lawrencejones/pg2sink/pkg/migration"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var logger = kitlog.NewLogfmtLogger(GinkgoWriter)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "pkg/imports/integration")
+}
