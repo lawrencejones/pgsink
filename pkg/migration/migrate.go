6a9c0324a5269c6965aa493bd0833594c26c5806
Lawrence Jones <lawrjone@gmail.com>
2020-05-25 13:34:01 +0100

    imports: re-add the error column
    This column was lost when the table was recreated- add it back, it's
    still useful.

--- pkg/migration/migrate.go
+++ pkg/migration/migrate.go
@@ -51,6 +51,13 @@ var _ = BeforeEach(func() {
 	}
 })
 
+// exec is a helper to clean-up most migrations, where we want to return the error given
+// by a SQL operation and no more.
+func exec(tx *sql.Tx, query string, args ...interface{}) error {
+	_, err := tx.Exec(query, args...)
+	return err
+}
+
 func Migrate(ctx context.Context, logger kitlog.Logger, db *sql.DB) error {
 	logger.Log("event", "schema.create", "schema", "pgsink")
 	if _, err := db.ExecContext(ctx, `create schema if not exists pgsink;`); err != nil {
