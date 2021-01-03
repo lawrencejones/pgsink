4f6395406c2293890c51d9e3b3333d3ef975fdb1
Lawrence Jones <lawrjone@gmail.com>
2021-01-03 20:21:09 +0000

    rm -rf pkg/util
    We no longer require this package, which is a nice change.

--- pkg/changelog/suite_test.go
+++ pkg/changelog/suite_test.go
@@ -1,4 +1,4 @@
-package util_test
+package changelog_test
 
 import (
 	"testing"
@@ -9,5 +9,5 @@ import (
 
 func TestSuite(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "pkg/util")
+	RunSpecs(t, "pkg/changelog")
 }
