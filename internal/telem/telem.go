ecf42594cdb0fa23a0d61579e6ab6d87484399c2
Lawrence Jones <lawrjone@gmail.com>
2021-01-10 17:18:27 +0000

    pkg/telem avoids double annotation
    Prevent us from adding two trace_id keys into the logger, by making the
    annotation idempotent based on the root context.

--- internal/telem/telem.go
+++ internal/telem/telem.go
@@ -10,11 +10,20 @@ import (
 // Logger can be used to tie logs to an on-going span. It is intended to wrap a
 // trace.StartSpan call, like so:
 //
-//   telem.Logger(trace.StartSpan(ctx, "pkg/imports.importer.Do"))
+//	telem.Logger(ctx, logger)(trace.StartSpan(ctx, "pkg/imports.importer.Do"))
 //
-// The logs will be decorated with a trace_id.
-func Logger(logger kitlog.Logger) func(context.Context, *trace.Span) (context.Context, *trace.Span, kitlog.Logger) {
+// The logs will be decorated with a trace_id. Root context is provided to avoid doubly
+// annotating the trace ID onto the same logger.
+func Logger(rootCtx context.Context, logger kitlog.Logger) func(context.Context, *trace.Span) (context.Context, *trace.Span, kitlog.Logger) {
 	return func(ctx context.Context, span *trace.Span) (context.Context, *trace.Span, kitlog.Logger) {
+		// If the root context already has a trace, assume our logger has been tagged and do
+		// nothing.
+		if trace.FromContext(rootCtx) != nil {
+			return ctx, span, logger
+		}
+
+		// If there's no span, we can assume no tracing is configured. No point annotating the
+		// logger with a nil trace ID.
 		if span == nil {
 			return ctx, span, logger
 		}
