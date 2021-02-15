689300ecbfca33ad9ffc9e4a0214fd8c2568b357
Lawrence Jones <lawrjone@gmail.com>
2021-02-15 17:33:57 +0000

    Fatal with the error on failure
    This means we see any errors that don't come out of the RunGroup, which
    is much better than failing silently (what broke?)

--- cmd/pgsink/main.go
+++ cmd/pgsink/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"os"
-
+	"github.com/alecthomas/kingpin"
 	"github.com/lawrencejones/pgsink/cmd/pgsink/cmd"
 )
 
 func main() {
 	if err := cmd.Run(); err != nil {
-		os.Exit(1)
+		kingpin.Fatalf(err.Error())
 	}
 }
